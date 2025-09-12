76387bf8cff6cca8063605d8a4a78cba05ddbc9d
johnjerrico <johnjerrico@users.noreply.github.com>
2025-09-12 15:39:52 +0700

    removing not needed function (#12)
    Co-authored-by: johannes.wijaya <johannes.wijaya@idexpress.com>

--- hantu_test.go
+++ hantu_test.go
@@ -20,8 +20,8 @@ func TestWorkerStart(t *testing.T) {
 		Max:    7,
 		TTL:    5 * time.Second,
 	})
-	bgworker.Worker().RegisterCommand("test",
-		func(ctx context.Context, request interface{}) {
+	bgworker.Worker().Register("test",
+		func(ctx context.Context, request any) {
 			for i := 0; i < 10000000000; i++ {
 				//do nothing
 			}
@@ -64,73 +64,6 @@ func TestWorkerStart(t *testing.T) {
 	bgworker.Worker().Stop()
 }
 
-/*
-func TestWorkerCancel(t *testing.T) {
-	//var wg sync.WaitGroup
-	//wg.Add(6)
-	bgworker := New(Option{
-		Domain:   "tests",
-		Id:       "1",
-		Max:      7,
-		TTL:      1 * time.Second,
-		Interval: 3 * time.Second,
-	})
-	bgworker.Worker().RegisterCommand("test",
-		func(ctx context.Context, request interface{}) {
-			select {
-			case <-ctx.Done():
-				return
-			default:
-				time.Sleep(20 * time.Second)
-				t.Log("Processing : " + fmt.Sprintf("%v", request))
-			}
-
-		},
-	)
-
-	bgworker.Worker().Start()
-	bgworker.Queue(schema.Job{
-		Id:       "1",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "1",
-	})
-	bgworker.Queue(schema.Job{
-		Id:       "2",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "2",
-	})
-	bgworker.Queue(schema.Job{
-		Id:       "3",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "3",
-	})
-	bgworker.Queue(schema.Job{
-		Id:       "4",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "4",
-	})
-	bgworker.Queue(schema.Job{
-		Id:       "5",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "5",
-	})
-	bgworker.Queue(schema.Job{
-		Id:       "6",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "6",
-	})
-	bgworker.Worker().Stop()
-	//wg.Wait()
-	time.Sleep(29 * time.Second)
-}
-*/
-
 func TestPanic(t *testing.T) {
 	var wg sync.WaitGroup
 	wg.Add(6)
@@ -142,23 +75,23 @@ func TestPanic(t *testing.T) {
 		Interval: 3 * time.Second,
 	})
 	cnt := 0
-	bgworker.Worker().RegisterCommand("test",
-		func(ctx context.Context, request interface{}) {
+	bgworker.Worker().Register("test",
+		func(ctx context.Context, request any) {
+			defer func() {
+				if r := recover(); r != nil {
+					wg.Done()
+				}
+			}()
 			select {
 			case <-ctx.Done():
 				return
 			default:
 				job := request.(*schema.Job)
-
 				if job.Id == "1" {
 					panic("test")
 				} else {
-					//time.Sleep(20 * time.Second)
 					fmt.Println("Hello", job.Id)
-					//t.Log("Processing : " + fmt.Sprintf("%v", request))
-
 				}
-
 			}
 			cnt++
 			wg.Done()
@@ -167,40 +100,34 @@ func TestPanic(t *testing.T) {
 
 	bgworker.Worker().Start()
 	bgworker.Queue(schema.Job{
-		Id:       "1",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "1",
+		Id:      "1",
+		Name:    "test",
+		Request: "1",
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "2",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "2",
+		Id:      "2",
+		Name:    "test",
+		Request: "2",
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "3",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "3",
+		Id:      "3",
+		Name:    "test",
+		Request: "3",
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "4",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "4",
+		Id:      "4",
+		Name:    "test",
+		Request: "4",
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "5",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "5",
+		Id:      "5",
+		Name:    "test",
+		Request: "5",
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "6",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "6",
+		Id:      "6",
+		Name:    "test",
+		Request: "6",
 	})
 	wg.Wait()
 	bgworker.Worker().Stop()
@@ -209,67 +136,6 @@ func TestPanic(t *testing.T) {
 	//time.Sleep(30 * time.Second)
 }
 
-/*
-	func TestWorkerChecksum(t *testing.T) {
-		var wg sync.WaitGroup
-		wg.Add(3)
-		bgworker := New(Option{
-			Domain:   "tests",
-			Id:       "1",
-			Max:      3,
-			TTL:      1 * time.Second,
-			Interval: 1 * time.Second,
-		})
-		bgworker.Worker().RegisterCommand("test",
-			func(ctx context.Context, request interface{}) {
-				select {
-				case <-ctx.Done():
-					wg.Done()
-				default:
-					t.Log("Processing : " + " -> " + fmt.Sprintf("%v", request))
-					time.Sleep(2 * time.Second)
-					wg.Done()
-				}
-
-			},
-		)
-		bgworker.Worker().RegisterChecksum(
-			"checksum",
-			func(ctx context.Context, checksum string, request []schema.Job) ([]schema.Job, []schema.Job, error) {
-				for _, val := range request {
-					if val.Id == "1" || val.Id == "2" {
-						return nil, nil, nil
-					}
-				}
-				return []schema.Job{
-					{
-						Id:       "1",
-						Name:     "test",
-						Checksum: "checksum",
-						Request:  "1",
-					},
-					{
-						Id:       "2",
-						Name:     "test",
-						Checksum: "checksum",
-						Request:  "2",
-					},
-					{
-						Id:       "3",
-						Name:     "test",
-						Checksum: "checksum",
-						Request:  "3",
-					},
-				}, nil, nil
-			},
-		)
-
-		bgworker.Worker().Start()
-		wg.Wait()
-		bgworker.Worker().Stop()
-
-}
-*/
 func TestDoubleQueue(t *testing.T) {
 	bgworker := New(Option{
 		Domain:   "tests",
@@ -279,16 +145,16 @@ func TestDoubleQueue(t *testing.T) {
 		Interval: 1 * time.Second,
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "1",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "1",
+		Id:   "1",
+		Name: "test",
+
+		Request: "1",
 	})
 	err := bgworker.Queue(schema.Job{
-		Id:       "1",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "1",
+		Id:   "1",
+		Name: "test",
+
+		Request: "1",
 	})
 	if err == nil {
 		t.Log("should be error due to duplicate")
@@ -306,16 +172,16 @@ func TestCountJobs(t *testing.T) {
 		Interval: 1 * time.Second,
 	})
 	bgworker.Queue(schema.Job{
-		Id:       "1",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "1",
+		Id:   "1",
+		Name: "test",
+
+		Request: "1",
 	})
 	err := bgworker.Queue(schema.Job{
-		Id:       "2",
-		Name:     "test",
-		Checksum: "checksum",
-		Request:  "2",
+		Id:   "2",
+		Name: "test",
+
+		Request: "2",
 	})
 	if err != nil {
 		t.Log("should not be error")
